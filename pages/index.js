d4e24f9b8d6fba0dab8d7d6732420c3873eecf14
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2021-10-25 09:49:23 +0100

    index update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,40 @@
+import { useState, setState } from "react";
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 export default function Home() {
-  return (<Button variant="contained">Hello World</Button>)
+  const [todo, setTodo] = useState('')
+  const [list, setList] = useState([])
+
+  const onEnter = (e) => {
+    const { keyCode } = e
+    if (keyCode === 13) {
+      addToList()
+    }
+  }
+
+  const addToList = () => {
+    setList(x => [...x, todo])
+    setTodo('')
+  }
+  
+  return (
+    <Container onKeyDown={onEnter}>
+      <header>
+        <TextField id="outlined-basic" margin="dense" label="Add a todo" variant="outlined" value={todo} onChange={(e) => setTodo(e.target.value)} />
+        <div>
+          <Button variant="contained" onClick={() => addToList()}>Add to list</Button>
+        </div>
+      </header>
+      <main>
+        <FormGroup>
+          {list.map((x, i) => <FormControlLabel control={<Checkbox />} key={i} label={x} />)}
+        </FormGroup>
+      </main>
+    </Container>
+  )
 }
